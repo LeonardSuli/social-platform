faad585250199b35d42dd50d1f3857afb99c0d92
LeonardSuli <leonardsuli1991@gmail.com>
2024-04-27 16:10:41 +0200

    add methods for class post

--- step_4_oop/index.php
+++ step_4_oop/index.php
@@ -15,10 +15,9 @@ class Post
     public $author;
     public $title;
     public $date;
-    // public $media;
 
     /**
-     * Constructor for the User class
+     * Constructor for the Post class
      * 
      * @param Int @id The post id
      * @param String @author The post author
@@ -33,8 +32,38 @@ class Post
         $this->title = $title;
         $this->date = $date;
     }
+
+    // Getter for $id
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    // Getter for $author
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    // Getter for $title
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    // Getter for $date
+    public function getDate()
+    {
+        return $this->date;
+    }
 }
 
 
 $post_1 = new Post(1, 'Mario Rossi', 'Finalmente si parte! Destinazione vacanze', '2022-11-22');
 var_dump($post_1);
+
+$post_2 = new Post(2, 'Mario Rossi', 'Una giornata di sole sulla spiaggia', '2022-09-15');
+var_dump($post_1);
+
+
+var_dump($post_1->getTitle());
