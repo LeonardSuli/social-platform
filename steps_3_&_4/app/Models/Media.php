42e3e7c1c941907bc71412e0e6b8f5894202d0a5
LeonardSuli <leonardsuli1991@gmail.com>
2024-05-06 17:18:56 +0200

    add children classes for class media

--- steps_3_&_4/app/Models/Media.php
+++ steps_3_&_4/app/Models/Media.php
@@ -45,3 +45,59 @@ class Media
         return $this->path;
     }
 }
+
+
+
+/**
+ * ### Class Video
+ * 
+ * @author Leonard Suli
+ * @license MIT
+ * @copyright 2024 Leonard Suli
+ */
+class Video extends Media
+{
+
+    /**
+     * Constructor for the Video class extends media
+     * 
+     * @param Int @id The media id
+     * @param String @type The media type
+     * @param String @path The media path
+     * @param String @resolution The video resolution
+     * @param Int @length The video length
+     * 
+     */
+    public function __construct(public int $id, public string $type, public string $path, public string $resolution, public int $length)
+    {
+        parent::__construct($id, $type, $path);
+        $this->resolution = $resolution;
+        $this->length = $length;
+    }
+}
+
+
+/**
+ * ### Class Photo
+ * 
+ * @author Leonard Suli
+ * @license MIT
+ * @copyright 2024 Leonard Suli
+ */
+class Photo extends Media
+{
+
+    /**
+     * Constructor for the Photo class extends media
+     * 
+     * @param Int @id The media id
+     * @param String @type The media type
+     * @param String @path The media path
+     * @param String @resolution The photo resolution
+     */
+    public function __construct(public int $id, public string $type, public string $path, public string $resolution)
+    {
+        parent::__construct($id, $type, $path);
+        $this->resolution = $resolution;
+    }
+}
